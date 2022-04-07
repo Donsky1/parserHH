f70588b524f961cba5a796e42f0a1d0e53869caa
Donsky1 <falson2007@yandex.ru>
2022-04-07 23:16:44 +0300

    Переписан сайт с использованием шаблонов

--- run.py
+++ run.py
@@ -7,35 +7,38 @@ import os
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
-@app.route('/#page-top', methods=['GET'])
-def index_get():
-    # with open('search_params.txt', 'r') as f:
-    #     params = f.read().split(',')
-    # params = [v.strip() for v in params]
-    return render_template('index.html')
+@app.route('/')
+def index_page():
+    name_page = 'Главная'
+    return render_template('index.html', title=name_page)
 
 
-@app.route('/', methods=['POST'])
-@app.route('/#page-top', methods=['POST'])
-def index_post():
-    if request.form['query_string']:
-        vacancy = request.form['query_string']
-        with_salary = True if request.form.getlist('with_salary') else False
-        print('post: ', vacancy, with_salary)
-        # time.sleep(5)
-        # df = pd.DataFrame()
-        # df.to_excel(os.path.join('static', 'docs', f'1.xlsx'), sheet_name=f'{1}')
-        # file_name = os.path.join('static', 'docs', f'1.xlsx')
-        file_name = parse_vacancy(vacancy, with_salary)
-        return render_template('index.html', flag=True, vacancy=vacancy, file=file_name)
+@app.route('/form/', methods=['GET', 'POST'])
+def form_page():
+    if request.method == 'GET':
+        return render_template('form.html')
     else:
-        return render_template('index.html')
+        if request.form['query_string']:
+            vacancy = request.form['query_string']
+            with_salary = True if request.form.getlist('with_salary') else False
+            print('post: ', vacancy, with_salary)
+            # time.sleep(5)
+            # df = pd.DataFrame()
+            # df.to_excel(os.path.join('static', 'docs', f'{vacancy}.xlsx'), sheet_name=f'{1}')
+            file_name = parse_vacancy(vacancy, with_salary)
+            return render_template('result.html', flag=1, vacancy=vacancy)
+        else:
+            return render_template('result.html', flag=3)
 
 
-@app.route('/load_page/')
-def load_page():
-    return render_template('load_page.html')
+@app.route('/result/')
+def result_page():
+    return render_template('result.html', flag=0)
+
+
+@app.route('/contacts/')
+def contacts_page():
+    return render_template('contacts.html')
 
 
 if __name__ == '__main__':
