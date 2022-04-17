797d4820e02fe6993da87b18684460d156350035
Donsky1 <falson2007@yandex.ru>
2022-04-18 01:35:18 +0300

    - исправлены основные критические ошибки - добавлены сессии - добавлена авторизация пользователей - дописан парсинг, в конце формируется xls файл, скачать который могут только зарегистрированные пользователи - добавлено динамическое поведение для некторых кнопок - добавлено уведомление сверху для зарегистрированных пользователей - изменен результат отображения парсинга

--- run.py
+++ run.py
@@ -1,23 +1,24 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session
 from parserhh import Vacancy, VacancyT, CompanyT, RegionT
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import os
+from users import UserT
 
 app = Flask(__name__)
 engine = create_engine('sqlite:///hhORM.sqlite', echo=False)
 Session = sessionmaker(bind=engine)
+app.secret_key = "BAD_SECRET_KEY"
 
 
 def get_5element_result(req, req_region):
-    session = Session()
-    n = session.query(VacancyT).filter(VacancyT.user_request == req, VacancyT.region == req_region).count()
+    sess = Session()
+    n = sess.query(VacancyT).filter(VacancyT.user_request == req, VacancyT.region == req_region).count()
     req_n = 5 if n > 5 else n
     result = []
-    for item in session.query(VacancyT).filter(VacancyT.user_request == req, VacancyT.region == req_region).all():
-        company_name = session.query(CompanyT).filter(CompanyT.id == item.company_id).first().name
+    for item in sess.query(VacancyT).filter(VacancyT.user_request == req, VacancyT.region == req_region).all():
+        company_name = sess.query(CompanyT).filter(CompanyT.id == item.company_id).first().name
         result.append([item.region, company_name, item.name, item.salary, item.link])
-    session.close()
+    sess.close()
     return result, req_n
 
 
@@ -31,27 +32,36 @@ def index_page():
 # Страница формы запроса
 @app.route('/form/', methods=['GET', 'POST'])
 def form_page():
-    session = Session()
     if request.method == 'GET':
-        regions = [region.name for region in session.query(RegionT).all()]
-        session.close()
+        sess = Session()
+        regions = [region.name for region in sess.query(RegionT).all()]
+        sess.close()
         return render_template('form.html', regions=regions)
     else:
         if request.form['query_string']:
-            vacancy = request.form['query_string']
-            region = request.form['select_region']
-            with_salary = True if request.form.getlist('with_salary') else False
-            searching = Vacancy(vacancy, region, with_salary)
-            print('post: ', vacancy, region, with_salary)
-            searching.parse_vacancy()
-            data, count_data = get_5element_result(req=vacancy, req_region=region)
-            with open(f'static/logs/{vacancy}({region})-log.txt', 'r', encoding='utf-8') as f:
-                src = f.readlines()
-            src = src[0:3] + src[8:10]
-            tmp_result = [item.replace('\n', '') for item in src]
-            session.close()
-            return render_template('result.html', flag=1, vacancy=vacancy, data=data, count_data=count_data,
-                                   itog=tmp_result)
+            try:
+                sess = Session()
+                vacancy = request.form['query_string']
+                region = request.form['select_region']
+                with_salary = True if request.form.getlist('with_salary') else False
+                searching = Vacancy(vacancy, region, with_salary)
+                print('form post: ', vacancy, region, with_salary)
+                result = searching.parse_vacancy()
+                if result == 0:
+                    print(f'Ничего не нашлось по вакансии {vacancy}  в регионе {region}')
+                    return render_template('result.html', flag=404)
+                data, count_data = get_5element_result(req=vacancy, req_region=region)
+                with open(f'static/logs/{vacancy}({region})-log.txt', 'r', encoding='utf-8') as f:
+                    src = f.readlines()
+                src = src[0:3] + src[8:10]
+                tmp_result = [item.replace('\n', '') for item in src]
+                searching.db_to_xls()
+                sess.close()
+                return render_template('result.html', flag=1, vacancy=vacancy, data=data, count_data=count_data,
+                                       itog=tmp_result)
+            except Exception as er:
+                print(er)
+                return render_template('result.html', flag=3)
         else:
             return render_template('result.html', flag=3)
 
@@ -68,10 +78,62 @@ def contacts_page():
     return render_template('contacts.html')
 
 
+def check_email(email):
+    sess = Session()
+    return True if sess.query(UserT).filter(UserT.email == email).count() == 1 else False
+
+
+def check_login(email, password):
+    sess = Session()
+    if sess.query(UserT).filter(UserT.email == email).first() is not None:
+        if sess.query(UserT).filter(UserT.password == password).first() is not None:
+            sess.close()
+            return True
+    sess.close()
+    return False
+
+
 # Страница регистрации
-@app.route('/login/')
+@app.route('/register/', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        sess = Session()
+        email = request.form['reg_email']
+        password = request.form['reg_password']
+        remember = True if request.form.getlist('reg_remember') else False
+        print('register post', email, password, remember)
+        if not check_email(email):
+            sess.add(UserT(str(email), password))
+            sess.commit()
+        else:
+            # тут нужно вывести уведомление на сайте
+            print('Пользователь c ником {} уже существует'.format(email))
+        if remember:
+            session['email'] = email
+        render_template('index.html')
+    return render_template('register.html')
+
+
+@app.route('/login/', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form['reg_email']
+        password = request.form['reg_password']
+        remember = True if request.form.getlist('reg_remember') else False
+        print('login post', email, password, remember)
+        if check_login(email, password):
+            session['email'] = email
+            return render_template('index.html')
+        else:
+            # тут нужно вывести уведомление на сайте
+            print('Введен неверный email или password')
     return render_template('login.html')
+
+
+@app.route('/exit/')
+def delete_visits():
+    session.pop('email', None)  # удаление данных о посещениях
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
