2c5bbfd39e8ad31965f022851371256bb9bf3382
Donsky1 <falson2007@yandex.ru>
2022-04-18 01:43:16 +0300

    - отсортирован порядок вывода регионов на форме

--- run.py
+++ run.py
@@ -35,7 +35,7 @@ def form_page():
     if request.method == 'GET':
         sess = Session()
         regions = [region.name for region in sess.query(RegionT).all()]
-        regions.sort()
+        regions.sort()  # сортировка регионов
         sess.close()
         return render_template('form.html', regions=regions)
     else:
